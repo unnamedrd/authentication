616f05fb8d42efedde8f4ef868228d9c0630869f
najwanan <theunnamedrd@gmail.com>
2022-07-12 23:53:16 -0700

    adding user registration

--- Auth/auth.js
+++ Auth/auth.js
@@ -1,2 +1,21 @@
 const user = require('.../model/user')
+const User = require('../model/user')
 
+exports.register = async (req, res, next) => {
+    const { username, password } = req.body
+    if (password.length < 6) {
+        return res.status(400).json({message: 'password less than six characters'})
+    }
+
+    try {
+        await User.create({
+            username, 
+            password, 
+        }).then(user =>
+            res.status(200).json({
+                message: "user successfully created", 
+                user, 
+            })
+        )
+    }catch(err)
+}
