2587b2371435f4f9f4cae47811793ee9828cb41e
najwanan <theunnamedrd@gmail.com>
2022-07-13 23:52:48 -0700

    adding auth route to server.js:

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ connectDB()
 
 app.use(express.json())
 
+app.use('/api/Auth', require('./Auth/Route')) //use statement for custom middleware we created in Auth
+
 const server = app.listen(PORT, () => console.log(`Server Connected to port ${PORT}`))
 
 process.on('unhandledRejection', err =>{
