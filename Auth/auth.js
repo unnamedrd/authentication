2625c3d995432f1ce8eebe67f016ffc62c92d402
najwanan <theunnamedrd@gmail.com>
2022-07-13 23:39:29 -0700

    adding catch err block to auth file

--- Auth/auth.js
+++ Auth/auth.js
@@ -1,24 +1,27 @@
 const User = require('.../model/user')
 const app = express()
 
-app.use(express.json())
-
 
 exports.register = async (req, res, next) => {
     const { username, password } = req.body
     if (password.length < 6) {
-        return res.status(400).json({message: 'password less than six characters'})
+        return res.status(400).json({ message: 'password less than six characters' })
     }
 
     try {
         await User.create({
-            username, 
-            password, 
+            username,
+            password,
         }).then(user =>
             res.status(200).json({
-                message: "user successfully created", 
-                user, 
+                message: "user successfully created",
+                user,
             })
         )
-    }catch(err)
+    } catch (err) {
+        res.status(401).json({
+            message: "user creation not successful",
+            error: error.message,
+        })
+    }
 }
