51d112ca006309b85a42e802bf59ef5de6a5de14
najwanan <theunnamedrd@gmail.com>
2022-07-21 23:20:27 -0700

    importing db file into server.js

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 const express = require('express')
 const app = express()
 const PORT = 5500
+const ConnectDB = require('./db')
+connectDB()
+const server = app.listen(PORT, ()=> console.log(`server connected to port ${PORT}`))
 
 let dbConnectionStr = process.env.DB_STRING,
 const connectDB = require('./db')
 
-connectDB()
+
 
 app.use(express.json())
 
 app.use('/api/Auth', require('./Auth/Route')) //use statement for custom middleware we created in Auth
-
-const server = app.listen(PORT, () => console.log(`Server Connected to port ${PORT}`))
 
 process.on('unhandledRejection', err =>{
     console.log(`An error occured: ${err.message}`)
